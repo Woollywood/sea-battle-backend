6585aa541601bb821e5bcacedf4075eea00c10f3
a.cherkashin <a.cherkashin@notamedia.com.ru>
2025-08-18 19:38:29 +0300

    feat: prettier

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common'
-import { AuthModule } from './auth/auth.module'
 import { ConfigModule } from '@nestjs/config'
+
+import { AuthModule } from './auth/auth.module'
 import { PrismaModule } from './prisma/prisma.module'
-import { UserModule } from './user/user.module'
 import { SessionModule } from './session/session.module'
+import { UserModule } from './user/user.module'
 
 @Module({
   imports: [
